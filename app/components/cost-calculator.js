8eda9c1465699736fed229b7426fc762ce9a085a
CJeezy <73773262+CJeezy@users.noreply.github.com>
2021-03-06 03:58:19 -0500

    added names

--- app/components/cost-calculator.js
+++ app/components/cost-calculator.js
@@ -1,5 +1,30 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { A as emberArray } from '@ember/array';
 
 export default class CostCalculatorComponent extends Component {
+    @tracked addedNameList = emberArray([])
+    @tracked currentAddedName = ''
+
+    constructor(){
+        super(...arguments)
+    }
+
+    @action
+    UpdateCurrentName(nameInput){
+        this.currentAddedName = nameInput.target.value
+    }
+
+    @action
+    AddName(){
+        this.addedNameList.addObject(this.currentAddedName)
+        this.currentAddedName = ''
+    }
+
+    @action
+    RemoveName(value){
+        this.addedNameList.removeObject(value)
+    }
 
 }
