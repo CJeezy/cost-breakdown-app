a3f9ddb26f24c475db709f2cc85f0711254f5ddb
CJeezy <73773262+CJeezy@users.noreply.github.com>
2021-03-06 00:46:06 -0500

    3/6 added cost-calculator border

--- app/components/person-counter.js
+++ app/components/person-counter.js
@@ -4,21 +4,22 @@ import { action } from '@ember/object';
 
 export default class PersonCounterComponent extends Component {
     @tracked count = 0;
+    @tracked personArray = []
 
     @action
     increment(){
         let temp = this.count
         if(temp < 10) this.count++
 
-        createForm()
+        this.personArray.length++
+    
     }
+
     @action
     decrement(){
         let temp = this.count
         if (temp !== 0) this.count--
-    }
 
-    createForm(){
-        
+        this.personArray.length--
     }
 }
